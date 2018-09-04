7ee1851148fc4d12fa37aaa2a26d3a5ddb42d347
Alex <alex.r.clark21@gmail.com>
2018-09-04 17:31:32 -0600

    ready for review

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import '../../App.css'
 
-function Product (props) {
-   let {index, productName, price, imageURL,} = props
+function Product(props) {
+    let { index, productName, price, imageURL, } = props
 
-console.log('product',props)
+    console.log('product', props)
     return (
         <div >
             <div>
-               <img 
-                    className = 'product_image'
+                <img
+                    className='product_image'
                     src={imageURL} />
                 <h2>{productName}</h2>
-                <h2>Price: ${price}</h2> 
+                <h2>Price: ${price}</h2>
                 <button
-                    onClick = {props.updateProduct}
+                    onClick={props.updateProduct}
                 >Edit</button>
                 <button
-                onClick = {() => {props.delete(index)}}
-                >Delete</button> 
+                    onClick={() => { props.delete(index) }}
+                >Delete</button>
             </div>
-            
-            <hr/>
+
+            <hr />
         </div>
     )
 }
